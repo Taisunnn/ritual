46af7201fe22bdbc20805726c5b97ef68d4fa113
Moon Kang <GYNj7@KTAvb*DSm&dPb%xX9Bjmd3fiq$2@5VA9xUXVT@@p*u9yvB2*Tf45gn^ket>
2023-12-01 17:27:58 -0500

    wip

--- game/controller/room_controller.py
+++ game/controller/room_controller.py
@@ -6,7 +6,7 @@ from game.model.room import Room
 class RoomController:
 
     def __init__(self):
-        rooms = []
+        self.rooms = []
         f=open(os.getcwd() + '/data/room.json')
         roomData = json.load(f)
         for room in roomData['rooms']:
